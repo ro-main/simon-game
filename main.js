de695ca514325cd88d7912673e58b2dd91e87f20
Romain Bouic <romainbouic@gmail.com>
2018-03-11 22:48:22 +0100

    setup + html css raw template

---
+++ main.js
@@ -0,0 +1,13 @@
+/* eslint-env browser */
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+/* eslint no-use-before-define: ["error", { "functions": false }] */
+
+function start() {
+  document.getElementById('buttons').addEventListener('click', () => {
+
+  });
+}
+
+document.getElementById('on-off').addEventListener('click', () => {
+  start();
+});
