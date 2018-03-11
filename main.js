3c9e8aad323c2c78b7eb0f535b047ffca59d378a
Romain Bouic <romainbouic@gmail.com>
2018-03-12 00:24:26 +0100

    on-off, start, computerPlays

--- main.js
+++ main.js
@@ -2,12 +2,69 @@
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 /* eslint no-use-before-define: ["error", { "functions": false }] */
 
-function start() {
-  document.getElementById('buttons').addEventListener('click', () => {
+const toReplay = [];
+const played = [];
+let gameOn = false;
 
+function onOff() {
+  if (gameOn) {
+    gameOn = false;
+    document.getElementById('start').removeEventListener('click', () => {
+      start();
+    });
+
+    document.getElementById('reset').removeEventListener('click', () => {
+      reset();
+    });
+  } else {
+    gameOn = true;
+    document.getElementById('start').addEventListener('click', () => {
+      start();
+    });
+
+    document.getElementById('reset').addEventListener('click', () => {
+      reset();
+    });
+  }
+}
+
+function reset() {
+
+}
+
+function start() {
+  computerPlays();
+  document.getElementById('buttons').addEventListener('click', (e) => {
+    const colorPlayed = e.target.id;
+    check(colorPlayed);
   });
 }
 
+function computerPlays() {
+  const chosenColor = choosingColor();
+  toReplay.push(chosenColor);
+  displayChosenColor(chosenColor);
+}
+
+function check() {
+
+}
+
+function choosingColor() {
+  const nb = Math.floor((Math.random() * 4) + 1);
+  if (nb === 1) {
+    return 'red';
+  } else if (nb === 2) {
+    return 'blue';
+  } else if (nb === 3) {
+    return 'green';
+  }
+  return 'yellow';
+}
+
+function displayChosenColor(color) {
+}
+
 document.getElementById('on-off').addEventListener('click', () => {
-  start();
+  onOff();
 });
