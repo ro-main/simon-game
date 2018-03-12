b756ecc4bdf2dc01ca0ef35a424824db53da2fde
Romain Bouic <romainbouic@gmail.com>
2018-03-12 23:44:02 +0100

    added sounds, displayChosenColor, playSound

--- main.js
+++ main.js
@@ -2,8 +2,18 @@
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 /* eslint no-use-before-define: ["error", { "functions": false }] */
 
+const redSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+const blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+const greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+const yellowSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+
+const redDefaultColor = 'rgba(255, 0, 0, 0.6)';
+const blueDefaultColor = 'rgba(0, 0, 255, 0.6)';
+const greenDefaultColor = 'rgba(0, 128, 0, 0.6)';
+const yellowDefaultColor = 'rgba(255, 255, 0, 0.6)';
+
 const toReplay = [];
-const played = [];
+// const played = [];
 let gameOn = false;
 
 function onOff() {
@@ -44,6 +54,7 @@ function computerPlays() {
   const chosenColor = choosingColor();
   toReplay.push(chosenColor);
   displayChosenColor(chosenColor);
+  playSound(chosenColor);
 }
 
 function check() {
@@ -63,6 +74,28 @@ function choosingColor() {
 }
 
 function displayChosenColor(color) {
+  document.getElementById(color).style.backgroundColor = color;
+  setTimeout(() => {
+    if (color === 'red') {
+      document.getElementById(color).style.backgroundColor = redDefaultColor;
+    } else if (color === 'blue') {
+      document.getElementById(color).style.backgroundColor = blueDefaultColor;
+    } else if (color === 'green') {
+      document.getElementById(color).style.backgroundColor = greenDefaultColor;
+    }
+    document.getElementById(color).style.backgroundColor = yellowDefaultColor;
+  }, 1000);
+}
+
+function playSound(color) {
+  if (color === 'red') {
+    redSound.play();
+  } else if (color === 'blue') {
+    blueSound.play();
+  } else if (color === 'green') {
+    greenSound.play();
+  }
+  yellowSound.play();
 }
 
 document.getElementById('on-off').addEventListener('click', () => {
